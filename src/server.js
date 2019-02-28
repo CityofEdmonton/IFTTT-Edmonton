662d5f8fad96eb3b50c981f782790abcd3a79730
Jared Rewerts <jaredrewerts@gmail.com>
2019-02-28 14:37:37 -0700

    Added static files.

--- src/server.js
+++ src/server.js
@@ -4,7 +4,12 @@ const airQuality = require('./controllers/air-quality')
 
 const port = 3000
 
+if (process.env.STATIC) {
+  app.use(express.static(process.env.static))
+}
+else {
+  app.use(express.static('public'))
+}
 app.use('/test/path', airQuality)
-app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
