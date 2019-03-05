4697c0643f2754dfe0cb2d57961c091ee0d83106
Jared Rewerts <jaredrewerts@gmail.com>
2019-03-05 11:06:49 -0700

    Allow passing objects into Redis functions.

--- src/cache/redis-cache.js
+++ src/cache/redis-cache.js
@@ -7,21 +7,29 @@ class RedisCache {
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT
     });
-    const hmsetAsync = promisify(client.hmset).bind(client);
-    const hgetAsync = promisify(client.hget).bind(client);
-    const hmgetAsync = promisify(client.hmget).bind(client);
+    this.hmsetAsync = promisify(client.hmset).bind(client);
+    this.hgetAsync = promisify(client.hget).bind(client);
+    this.hmgetAsync = promisify(client.hmget).bind(client);
+    this.hgetallAsync = promisify(client.hgetall).bind(client);
   }
 
   async hget(key, subkey) {
-    return await hgetAsync(key, subkey)
+    return await this.hgetAsync(key, subkey)
   }
 
-  async hmset(...args) {
-    return await hmsetAsync(args)
+  async hmset(key, args) {
+    return await this.hmsetAsync(key, ...this.objToArray(args))
   }
 
-  async hmget(...args) {
-    return await hmgetAsync(...args)
+  async hgetall(key) {
+    return await this.hgetallAsync(key)
+  }
+
+  objToArray(obj) {
+    let val = Object.keys(obj).reduce((r, k) => {
+      return r.concat(k, obj[k]);
+    }, []);
+    return val
   }
 }
 
