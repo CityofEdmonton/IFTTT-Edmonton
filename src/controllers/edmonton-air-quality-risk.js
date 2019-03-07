f14649ad2ef51f741f4a5a14275d7d0e48a73c96
Jared Rewerts <jaredrewerts@gmail.com>
2019-03-07 11:22:07 -0700

    Cleaned comments for edmonton air quality route.

--- src/controllers/edmonton-air-quality-risk.js
+++ src/controllers/edmonton-air-quality-risk.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const handleAQHI = require('./aqhi-base')
 
-// Home page route.
+/**
+ * Handles health risk changes for Edmonton (#67).
+ * This route requires no triggerFields.
+ */
 router.post('/', async function (req, res) {
   await handleAQHI(req, res, 'health_risk', '67')
 })
