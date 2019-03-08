ecde3f8a59713c253d322600b5e075111a9df095
Jared Rewerts <jaredrewerts@gmail.com>
2019-03-08 09:58:00 -0700

    Switched to using new cache for air quality.

--- src/controllers/aqhi-base.js
+++ src/controllers/aqhi-base.js
@@ -28,69 +28,36 @@ module.exports = async function handleAQHI(req, res, field, communityID) {
       continue
     }
 
+    let id = uuid()
+    stationAirQuality['id'] = id
+    stationAirQuality['date_created'] = (new Date()).toString()
+    stationAirQuality['meta'] = {
+      id,
+      timestamp: Math.round((new Date()) / 1000)
+    }
+
     let { community_name, aqhi_current, health_risk } = stationAirQuality
     console.log(`${community_name} has a ${health_risk} risk.`)
     let key = `${community_name}/${field}`
-    let metaKey = `${community_name}/${field}/meta`
 
     // Set base object info
     console.log(`Key for cache is ${key}`)
-    let oldField = await req.cache.hget(key, field)
-    if (oldField === stationAirQuality[field]) {
+    let latest = await req.cache.getLatest(key)
+    if (latest && latest[field] === stationAirQuality[field]) {
       console.log('Sending old data.')
-      let oldData = await req.cache.hgetall(key)
-      let meta = await req.cache.hgetall(metaKey)
-      oldData['meta'] = meta
+      let oldData = await req.cache.getAll(key)
       return res.send({
         data: oldData
       })
     }
-    else if (oldField && oldField !== stationAirQuality[field]) {
-      console.log('Updating existing.')
-      let oldData = await req.cache.hgetall(key)
-      let oldMeta = await req.cache.hgetall(metaKey)
-
-      let id = uuid()
-      let newData = {...oldData, ...stationAirQuality}
-      newData['id'] = id
-      newData['date_created'] = (new Date()).toString()
-      await req.cache.hmset(key, newData)
-      
-      let newMeta = {
-        id,
-        timestamp: Math.round(new Date() / 1000)
-      }
-      await req.cache.hmset(metaKey, newMeta)
-
-
-      newData['meta'] = newMeta
-      oldData['meta'] = oldMeta
-      return res.send({
-        data: [
-          newData,
-          oldData
-        ]
-      })
-    }
     else {
-      console.log('New request')
-      let id = uuid()
-      stationAirQuality['date_created'] = new Date()
-      stationAirQuality['id'] = id
-      await req.cache.hmset(key, stationAirQuality)
-
-      let meta = {
-        id,
-        timestamp: Math.round(stationAirQuality['date_created'] / 1000)
-      }
-      await req.cache.hmset(metaKey, meta)
-
-      stationAirQuality['meta'] = meta
+      console.log('Updating old data.')
+      await req.cache.add(key, stationAirQuality)
+      let logs = await req.cache.getAll(key)
       return res.send({
-        data: stationAirQuality
+        data: logs
       })
     }
-    // Set object meta info
   }
   return res.send(404, `No communities found with ID ${communityID}`)
 }
