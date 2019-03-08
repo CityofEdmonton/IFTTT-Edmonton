299af942debad9609d80489fce3b6651dee2b7df
Jared Rewerts <jaredrewerts@gmail.com>
2019-03-08 11:11:37 -0700

    Updated error message to meet IFTTT standard.

--- src/middleware/ifttt-validator.js
+++ src/middleware/ifttt-validator.js
@@ -2,7 +2,11 @@ module.exports = function (req, res, next) {
   if (req.headers['ifttt-service-key'] != process.env.IFTTT_SERVICE_KEY) {
     let message = 'Invalid IFTTT service key provided.'
     console.error(message)
-    return res.status(401).send(message)
+    return res.status(401).send({
+      errors: [{
+        message
+      }]
+    })
   }
   next()
 }
