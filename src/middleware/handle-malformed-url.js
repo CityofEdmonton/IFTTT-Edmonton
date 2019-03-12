cf5ba37954b6222b212eac1b6e7222228753d485
Jared Rewerts <jaredrewerts@gmail.com>
2019-03-12 16:54:49 -0600

    Added middleware for handling malformed URLs
    IFTTT is sending weird URLs.

---
+++ src/middleware/handle-malformed-url.js
@@ -0,0 +1,21 @@
+/**
+ * Remove leading slashes from the original URL.
+ * Example: https://webhost.ca//ifttt/v1/triggers/light_the_bridge
+ * becomes https://webhost.ca/ifttt/v1/triggers/light_the_bridge
+ * This fixes an issue where IFTTT will pass through a URL that 
+ * has too many slashes.
+ */
+module.exports = function (req, res, next) {
+  let leadingSlashes = 0
+  let splits = req.originalUrl.split('/').filter((item) => {
+    if (item == '') {
+      leadingSlashes++
+      return false
+    }
+    return true
+  })
+
+  let url = `${req.protocol}://${req.get('host')}/${splits.join('/')}`
+  req.url = req.originalUrl = `/${splits.join('/')}`
+  next()
+}
