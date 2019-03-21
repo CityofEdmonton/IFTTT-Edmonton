9eb70d52bde152b8e78a8fae70e57ae0530b617d
Henry <wjxhenry@gmail.com>
2019-03-21 15:11:02 -0600

    Colour parsing using lodash and colornames npm packages

--- src/controllers/light-the-bridge.js
+++ src/controllers/light-the-bridge.js
@@ -1,14 +1,23 @@
 const request = require('request-promise-native')
 const parseXML = require('../utils/parse-ltb-xml')
+var _ = require('lodash')
+var toHex = require('colornames')
 
 /**
  * Returns the most recent light the bridge results to IFTTT.
  */
 module.exports = async function(req, res) {
   let tweet
+  var colors = []
   try {
     let xmlString = await request(process.env.LTB_URL)
     tweet = await parseXML(xmlString)
+    // Parse colors
+    const words = _.words(tweet.title)
+    for (let i = 0; i < words.length; i++) {
+      var hexColor = toHex(words[i])
+      hexColor ? colors.push({color: words[i], hex: hexColor}) : null
+    }
   } catch (e) {
     console.error(e)
     return res.status(500).send({
@@ -25,17 +34,19 @@ module.exports = async function(req, res) {
     ...tweet,
     ...{
       created_at: new Date().toISOString(),
-      color_description: '',
-      color1: '',
-      color2: '',
-      color3: '',
-      color4: '',
+      color_description: colors.map(function(element) { return element.color }).join(', '),
+      color1: colors[0].hex,
+      color2: colors[1].hex,
+      color3: colors[2].hex,
+      color4: colors[3].hex,
       meta: {
         id: tweet.id,
         timestamp: Math.round(new Date() / 1000)
       }
     }
   }
+
+  console.log(tweet)
 
   let responseValues
   let key = `light_the_bridge`
