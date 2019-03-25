4d86953dd0a506cc9cc223a82a617e552e2ef4be
WJXHenry <wjxhenry@gmail.com>
2019-03-25 16:59:30 -0600

    Datasets field for applet trigger

--- src/controllers/open-data-datasets.js
+++ src/controllers/open-data-datasets.js
@@ -1,13 +1,18 @@
 const request = require('request-promise-native')
 
+function sortOrder(a, b) {
+  return a.label < b.label ? -1 : (a.label > b.label ? 1 : 0)
+}
+
 module.exports = async function(req, res) {
   let datasets
   try {
     let rawJsonData = await request(process.env.OPEN_DATA_URL)
     const rawData = JSON.parse(rawJsonData)
     datasets = rawData.dataset.map(function(entry){
-        return entry.title
-    }).sort()
+        return { label: entry.title, value: entry.identifier }
+    }).sort(sortOrder)
+    console.log(datasets)
   } catch (e) {
     e.code = 500
     throw e
