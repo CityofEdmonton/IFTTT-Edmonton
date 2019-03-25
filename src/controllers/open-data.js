230949e724be3210a9fa69699aa2e42181d5eecb
Henry <wjxhenry@gmail.com>
2019-03-25 12:29:26 -0600

    Starting work on controller for open data

---
+++ src/controllers/open-data.js
@@ -0,0 +1,21 @@
+const request = require('request-promise-native')
+const parseXML = require('../utils/parse-aqhi-locations')
+
+/**
+ * This route returns the cities and their community IDs
+ * for use as a dynamic trigger field.
+ */
+module.exports = async function(req, res) {
+  let cities
+  try {
+    // let xmlString = await request(process.env.AIR_QUALITY_URL)
+    // cities = await parseXML(xmlString)
+    cities = await request(process.env.OPEN_DATA_URL)
+  } catch (e) {
+    e.code = 500
+    throw e
+  }
+  res.status(200).send({
+    data: cities
+  })
+}
