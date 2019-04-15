ef1aa88c795b6ed3702f41d3b632176672790d34
Henry <wjxhenry@gmail.com>
2019-04-15 09:56:36 -0600

    Filter code example

---
+++ src/utils/odp-applet-filter-code.js
@@ -0,0 +1,25 @@
+function sortNumber(a: number, b: number) {
+  return a - b
+}
+
+if (
+  CityOfEdmontonHenryStaging.openData.DataSet ==
+  'LRT Elevator/Escalator Outages'
+) {
+  const columnValues = JSON.parse(
+    CityOfEdmontonHenryStaging.openData.ColumnValues
+  ).sort(sortNumber)
+  const outages = JSON.parse(CityOfEdmontonHenryStaging.openData.AllValues)
+    .map((entry: any) => {
+      return ` ${entry.lrt_station_name}: ${entry.lrt_device_location} `
+    })
+    .sort()
+
+  Email.sendMeEmail.setSubject('LRT Elevator/Escalator Outages')
+  Email.sendMeEmail.setBody(
+    `Column being watched: ${CityOfEdmontonHenryStaging.openData.Column} ||
+    Returned column values: ${columnValues} ||
+    Number of updated columns: ${columnValues.length} ||
+    Outages: ${outages}`
+  )
+}
