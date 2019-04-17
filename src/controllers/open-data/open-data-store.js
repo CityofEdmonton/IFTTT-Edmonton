68304a82c5d008bc37ef9e8b973384a61a9fb216
WJXHenry <wjxhenry@gmail.com>
2019-04-17 10:49:07 -0600

    key for sending data store request

--- src/controllers/open-data/open-data-store.js
+++ src/controllers/open-data/open-data-store.js
@@ -5,14 +5,21 @@ const { storeData } = require('../../utils/store-odp-data')
  * and store the dataset data into the Redis cache
  */
 module.exports = async function(req, res) {
-  try {
-    storeData(req.store, 185) // Only datasets that were last modified half a year ago or less
-    // storeData(req.store)
-  } catch (e) {
-    e.code = 500
-    throw e
+  // If no environment variable is set, the request will not need a key
+  if (req.headers['ifttt-data-store-key'] == process.env.IFTTT_DATA_STORE_KEY) {
+    try {
+      storeData(req.store, 185) // Only datasets that were last modified half a year ago or less
+      // storeData(req.store)
+    } catch (e) {
+      e.code = 500
+      throw e
+    }
+    res.status(200).send({
+      data: 'Request sent.'
+    })
+  } else {
+    res.status(401).send({
+      error: 'Invalid Date-store key provided. Unable to send request.'
+    })
   }
-  res.status(200).send({
-    data: 'Request sent.'
-  })
 }
