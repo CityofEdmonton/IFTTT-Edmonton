8b0ae3d227a1a91f2176dccece4b272c6d0b83cc
j-rewerts <jaredrewerts@gmail.com>
2019-04-25 15:03:54 -0600

    Remove in memory cache.

--- src/middleware/cache-provider.js
+++ src/middleware/cache-provider.js
@@ -3,17 +3,11 @@
  * This is a poor (wo)mans dependency injection.
  * @author j-rewerts
  */
-const Cache = require('../in-mem-cache')
 const RedisCache = require('../cache/redis-cache')
 const ChangeWriter = require('../cache/change-writer')
 
-let cache
-if (process.env.CACHE == 'REDIS') {
-  let redis = new RedisCache()
-  cache = new ChangeWriter(redis, process.env.MAX_RESULTS)
-} else {
-  cache = new Cache()
-}
+let redis = new RedisCache()
+let cache = new ChangeWriter(redis, process.env.MAX_RESULTS)
 
 var cacheProvider = function(req, res, next) {
   req.cache = cache
