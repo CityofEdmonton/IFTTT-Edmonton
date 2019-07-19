ea34f43573d91206a8b6e88d570f9e91af1aa31b
WJXHenry <wjxhenry@gmail.com>
2019-07-19 14:44:26 -0600

    Moved average water level calculation to SoQL queries

--- src/controllers/water-level-changes.js
+++ src/controllers/water-level-changes.js
@@ -1,7 +1,7 @@
 const request = require('request-promise-native')
 const uuid = require('uuid/v4')
 
-const TIMEZONE_OFFSET_MILLIS = 360 * 60 * 1000 // Timezone offset for Edmonton to UTC
+const TIMEZONE_OFFSET_MILLIS = -1 * 360 * 60 * 1000 // Timezone offset for Edmonton to UTC
 const KEY = 'water-level-changes' // Unique key for dataset storage
 const URL = 'https://data.edmonton.ca/resource/cnsu-iagr.json' // The Socrata api endpoint
 const QUERY_BASE = `${URL}?$query=`
@@ -30,7 +30,7 @@ module.exports = async function(req, res) {
     let rawJsonUpdated = await request(getUpdatedQuery)
     updatedData = JSON.parse(rawJsonUpdated)
     recentUpdated = updatedData[0].date_and_time
-    recentWaterLevel = updatedData[0].water_level_m
+    recentWaterLevel = Number(updatedData[0].water_level_m)
   } catch (e) {
     e.code = 500
     throw e
@@ -42,18 +42,46 @@ module.exports = async function(req, res) {
     responseValues = await req.cache.getAll(KEY, req.body['limit'])
   } else {
     console.log('Dataset was updated')
-    let oneDayBounds = getBounds(recentUpdated, 1)
-    let threeDaysBounds = getBounds(recentUpdated, 3)
-    let oneWeekBounds = getBounds(recentUpdated, 7)
-    let oneDayResults
-    let threeDaysResults
-    let oneWeekResults
+    let currentDay = getBounds(recentUpdated, {
+      timeZoneOffset: TIMEZONE_OFFSET_MILLIS
+    })
+    let previousDay = getBounds(recentUpdated, {
+      timeZoneOffset: TIMEZONE_OFFSET_MILLIS,
+      offset: -1
+    })
+    let threeDaySpan = getBounds(recentUpdated, {
+      timeZoneOffset: TIMEZONE_OFFSET_MILLIS,
+      offset: -2,
+      span: true
+    })
+    let threeDaysAgo = getBounds(recentUpdated, {
+      timeZoneOffset: TIMEZONE_OFFSET_MILLIS,
+      offset: -2
+    })
+    let oneWeekSpan = getBounds(recentUpdated, {
+      timeZoneOffset: TIMEZONE_OFFSET_MILLIS,
+      offset: -6,
+      span: true
+    })
+    let oneWeekAgo = getBounds(recentUpdated, {
+      timeZoneOffset: TIMEZONE_OFFSET_MILLIS,
+      offset: -6
+    })
+    let currentDayAvg
+    let previousDayAvg
+    let threeDaySpanAvg
+    let threeDaysAgoAvg
+    let oneWeekSpanAvg
+    let oneWeekAgoAvg
     try {
-      await Promise.all(
-        (oneDayResults = await getData(oneDayBounds)),
-        (threeDaysResults = await getData(threeDaysBounds)),
-        (oneWeekResults = await getData(oneWeekBounds))
-      )
+      await Promise.all([
+        (currentDayAvg = await queryData(currentDay)),
+        (previousDayAvg = await queryData(previousDay)),
+        (threeDaySpanAvg = await queryData(threeDaySpan)),
+        (threeDaysAgoAvg = await queryData(threeDaysAgo)),
+        (oneWeekSpanAvg = await queryData(oneWeekSpan)),
+        (oneWeekAgoAvg = await queryData(oneWeekAgo))
+      ])
     } catch (e) {
       e.code = 500
       throw e
@@ -64,13 +92,19 @@ module.exports = async function(req, res) {
       id,
       created_at: new Date(Date.now()).toISOString(),
       last_updated: recentUpdated,
-      latest_water_level: recentWaterLevel,
-      one_day_average: oneDayResults[0].toFixed(3),
-      one_day_delta: oneDayResults[1].toFixed(3),
-      three_day_average: threeDaysResults[0].toFixed(3),
-      three_day_delta: threeDaysResults[1].toFixed(3),
-      one_week_average: oneWeekResults[0].toFixed(3),
-      one_week_delta: oneWeekResults[1].toFixed(3),
+      latest_water_level: recentWaterLevel.toFixed(3),
+      one_day_average: currentDayAvg.averageWaterLevel.toFixed(3),
+      one_day_delta: (
+        currentDayAvg.averageWaterLevel - previousDayAvg.averageWaterLevel
+      ).toFixed(3),
+      three_day_average: threeDaySpanAvg.averageWaterLevel.toFixed(3),
+      three_day_delta: (
+        currentDayAvg.averageWaterLevel - threeDaysAgoAvg.averageWaterLevel
+      ).toFixed(3),
+      one_week_average: oneWeekSpanAvg.averageWaterLevel.toFixed(3),
+      one_week_delta: (
+        currentDayAvg.averageWaterLevel - oneWeekAgoAvg.averageWaterLevel
+      ).toFixed(3),
       meta: {
         id,
         timestamp: Math.round(new Date() / 1000)
@@ -86,50 +120,67 @@ module.exports = async function(req, res) {
 }
 
 /**
- * This function obtains the upper and lower bounds of a time period
+ * This function obtains the upper and lower bounds of a the full day of the given date, or it can be used
+ * to get the bounds of date ranges.
  * @param {String} date The date in ISO format
- * @param {Number} days The number of days the period should be
- * @returns {Array<String>} The upper and lower bounds of the time period
+ * @param {Object} [options] The options for the bounds
+ * @param {Number} [options.timeZoneOffset] The time zone offset of the date (optional)
+ * @param {Number} [options.offset] The number of days to offset the current date by (optional)
+ * @param {Boolean} [options.span] If enabled, will return the bounds between the specified date and offset (default: false)
+ * @returns {Array<String>} The upper and lower bounds of the full day
  */
-function getBounds(date, days) {
-  const dateRegex = /[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}/
-  let endOfPeriod = new Date(new Date(date) - TIMEZONE_OFFSET_MILLIS)
-  let startOfPeriod = new Date(
-    new Date(date) - (days * 24 * 60 * 60 * 1000 + TIMEZONE_OFFSET_MILLIS)
-  )
-  let lowerBound = startOfPeriod.toISOString().match(dateRegex)[0]
-  let upperBound = endOfPeriod.toISOString().match(dateRegex)[0]
+function getBounds(date, options) {
+  const dateRegex = /[0-9]{4}-[0-9]{2}-[0-9]{2}/
+  let timeZoneOffset = options.timeZoneOffset || 0
+  let offset = options.offset || 0
+  let span = options.span || false
+  let dayOffset = offset * 24 * 60 * 60 * 1000
+  let dateObject = new Date(date)
+  let referenceDay = new Date(dateObject.getTime() + timeZoneOffset)
+  let day = new Date(dateObject.getTime() + timeZoneOffset + dayOffset)
+  let lowerBound
+  let upperBound
+  if (span) {
+    if (referenceDay > day) {
+      lowerBound = day.toISOString().match(dateRegex)[0] + 'T00:00:00.000'
+      upperBound =
+        referenceDay.toISOString().match(dateRegex)[0] + 'T23:59:59.999'
+    } else {
+      lowerBound =
+        referenceDay.toISOString().match(dateRegex)[0] + 'T00:00:00.000'
+      upperBound = day.toISOString().match(dateRegex)[0] + 'T23:59:59.999'
+    }
+  } else {
+    let returnDay = day.toISOString().match(dateRegex)[0]
+    lowerBound = returnDay + 'T00:00:00.000'
+    upperBound = returnDay + 'T23:59:59.999'
+  }
   return [lowerBound, upperBound]
 }
 
 /**
- * Returns the average and delta of the water levels respecitvely in an array
+ * Queries the API endpoint for the average water level in meters between the specified bounds and the
+ * number of data points within that bound.
  * @param {Array<String>} bounds The bounds to do calculation on (dates in ISO 8601 format)
- * @returns {Array<Number>} The average and delta of the water level between the specified bounds
+ * @returns {Object} The number of data points and average water level between the specified bounds: { dataCount, averageWaterLevel }
  */
-async function getData(bounds) {
-  let workingDataQuery = `${QUERY_BASE}
-    SELECT water_level_m
+async function queryData(bounds) {
+  let dataQuery = `${QUERY_BASE}
+    SELECT count(*), avg(water_level_m)
     WHERE station_number = '${STATION_NUMBER}'
     AND date_and_time
     BETWEEN '${bounds[0]}'
-    AND '${bounds[1]}'
-    ORDER BY date_and_time DESC
-    LIMIT 10000`
-  let workingData
+    AND '${bounds[1]}'`
+  let queryResults
   try {
-    let rawJsonData = await request(workingDataQuery)
-    workingData = JSON.parse(rawJsonData).map(data => {
-      return Number(data.water_level_m)
-    })
+    let rawQueryResults = await request(dataQuery)
+    queryResults = JSON.parse(rawQueryResults)[0]
   } catch (e) {
     e.code = 500
     throw e
   }
-  let delta = workingData[0] - workingData[workingData.length - 1]
-  let sum = workingData.reduce(function(a, b) {
-    return a + b
-  })
-  let average = sum / workingData.length
-  return [average, delta]
+  return {
+    dataCount: Number(queryResults.count),
+    averageWaterLevel: Number(queryResults.avg_water_level_m)
+  }
 }
